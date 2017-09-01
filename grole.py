c51664ce39285da6880159397862d6f677730f78
witchard <witchard@hotmail.co.uk>
2017-09-01 20:11:44 +0000

    Extracted request handling to a class

--- grole.py
+++ grole.py
@@ -1,33 +1,54 @@
 import asyncio
 import socket
-import pprint
 
-async def readline(reader):
-    ret = await reader.readline()
-    if len(ret) == 0 and reader.at_eof():
-        raise EOFError()
-    return ret
+class Request:
+    """
+    Handles a single HTTP request
+    """
+    def __init__(self, reader):
+        """
+        Initialise
+        """
+        self.reader = reader
 
-async def get_header(reader):
-    start_line = await readline(reader)
-    method, location, version = start_line.decode().split()
-    headers = {}
-    while True:
-        header_raw = await readline(reader)
-        if header_raw.strip() == b'':
-            break
-        header = header_raw.decode().split(':', 1)
-        headers[header[0]] = header[1].strip()
-    return {'method': method, 'location': location, 'version': version,
-            'headers': headers}
+    async def _init(self):
+        """
+        Perform initialisation
+        Parses HTTP request headers, this needs to be async so we can't do it in
+        init.
+        """
+        start_line = await self._readline()
+        self.method, self.location, self.version = start_line.decode().split()
+        self.headers = {}
+        while True:
+            header_raw = await self._readline()
+            if header_raw.strip() == b'':
+                break
+            header = header_raw.decode().split(':', 1)
+            self.headers[header[0]] = header[1].strip()
+
+        # TODO implement chunked handling
+        self._content_remaining = self.headers.get('Content-Length', 0)
+        self._buffered = ''
+
+    async def _readline(self):
+        """
+        Readline helper
+        """
+        ret = await self.reader.readline()
+        if len(ret) == 0 and self.reader.at_eof():
+            raise EOFError()
+        return ret
 
 async def handle_echo(reader, writer):
     peer = writer.get_extra_info('peername')
     print('New connection from {}'.format(peer))
     try:
         while True:
-            req = await get_header(reader)
-            pprint.pprint(req)
+            req = Request(reader)
+            await req._init()
+            print(req.method, req.location, req.version)
+            print(req.headers)
             writer.write(b'HTTP/1.1 404 Not Found\r\nServer: grole/0.1\r\nContent-Length: 0\r\n\r\n')
             await writer.drain()
             print('Handled request')
