327a80fafed341faf394a29815482f7051423ddf
witchard <witchard@hotmail.co.uk>
2017-09-23 17:39:41 +0100

    Add logging to command line args

--- test/test_args.py
+++ test/test_args.py
@@ -10,11 +10,21 @@ class TestArgs(unittest.TestCase):
         self.assertEqual(args.port, 1234)
         self.assertEqual(args.directory, '.')
         self.assertEqual(args.noindex, False)
+        self.assertEqual(args.verbose, False)
+        self.assertEqual(args.quiet, False)
 
     def test_override(self):
-        args = grole.parse_args(['-a', 'foo', '-p', '27', '-d', 'bar', '-n'])
+        args = grole.parse_args(['-a', 'foo', '-p', '27', '-d', 'bar', '-n', '-v'])
         self.assertEqual(args.address, 'foo')
         self.assertEqual(args.port, 27)
         self.assertEqual(args.directory, 'bar')
         self.assertEqual(args.noindex, True)
+        self.assertEqual(args.verbose, True)
+        self.assertEqual(args.quiet, False)
 
+    def test_error(self):
+        try:
+            grole.parse_args(['-q', '-v'])
+        except SystemExit:
+            return
+        self.fail('Did not error on mutually exclusive args')
