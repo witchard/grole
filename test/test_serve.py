20696d6f236afc1bc0e2b3db570363540e70ca84
witchard <witchard@hotmail.co.uk>
2017-11-17 01:56:28 +0000

    Use ip instead of localhost for travis

--- test/test_serve.py
+++ test/test_serve.py
@@ -14,7 +14,7 @@ def simple_server():
     def hello(env, req):
         return 'Hello, World!'
 
-    app.run()
+    app.run(host='127.0.0.1')
 
 class TestServe(unittest.TestCase):
 
@@ -22,16 +22,16 @@ class TestServe(unittest.TestCase):
         p = multiprocessing.Process(target=simple_server)
         p.start()
         time.sleep(0.1)
-        with urllib.request.urlopen('http://localhost:1234') as response:
+        with urllib.request.urlopen('http://127.0.0.1:1234') as response:
                html = response.read()
                self.assertEqual(html, b'Hello, World!')
         p.terminate()
 
     def test_fileserver(self):
-        p = multiprocessing.Process(target=grole.main, args=[[]])
+        p = multiprocessing.Process(target=grole.main, args=[['-a', '127.0.0.1']])
         p.start()
         time.sleep(0.1)
-        with urllib.request.urlopen('http://localhost:1234/test/test.dat') as response:
+        with urllib.request.urlopen('http://127.0.0.1:1234/test/test.dat') as response:
                html = response.read()
                self.assertEqual(html, b'foo\n')
         p.terminate()
